41213043ec7e1aa968216c86b2a464ca6a4549ae
QMaximillian <quinnlashinsky@gmail.com>
2019-03-16 23:34:45 -0400

    First steps using Formik

--- src/components/ActorProfile.js
+++ src/components/ActorProfile.js
@@ -1,167 +1,237 @@
 import React, { Component } from 'react'
+import { Formik, FormikProps, Form, Field, ErrorMessage } from "formik";
 import { connect } from 'react-redux'
 import { loadInitialActorState, updateCurrentActorForm } from '../actions/actions'
 import { fetchUpdateCurrentActor } from '../adapters/actorAdapter'
-import { Loader, Form, Input, Select, Button, Grid } from 'semantic-ui-react'
+// import { Loader, Form, Input, Select, Button, Grid } from 'semantic-ui-react'
 import '../Audition.css'
 import withAuth from '../hocs/withAuth'
 
 
 
-class ActorProfile extends Component {
+// class ActorProfile extends Component {
 
-  state = {
-    success: false
+//   state = {
+//     success: false
+//   }
+
+//   handleChange = (event) => {
+//     event.persist();
+//     if (event.name) {
+//       this.props.updateCurrentActorForm({
+//         [event.name]: event.value
+//       })
+//     } else {
+//       this.props.updateCurrentActorForm({
+//         [event.target.name]: event.target.value
+//       })
+//     }
+//   }
+
+
+//   updateActor = (event) => {
+//     event.preventDefault()
+
+//     const { first_name, last_name, email, height, vocal_range, equity, gender, birthday, city } = this.props.currentActor.attributes
+
+//       fetchUpdateCurrentActor(this.props.currentActor.id, {first_name, last_name, email, height, vocal_range, equity, gender, birthday, city }).then(
+//         this.setState({ success: !this.state.success }, () => {
+//           alert('Profile Saved')
+//           this.setState({ success: !this.state.success })
+//         })
+//       )
+//     }
+
+
+//   render() {
+
+//     const { first_name, last_name, email, feet, inches, vocal_range, equity, gender, birthday } = this.props.currentActor.attributes
+
+//     const booleans = [
+//       { key: 'true', text: "True", value: true },
+//       { key: 'false', text: "False", value: false }
+//     ]
+
+//     const genders = [
+//       {key: 'female', text: "Female", value: "Female"},
+//       {key: 'male', text: "Male", value: "Male"},
+//       {key: 'Transgender', text: "Transgender", value: "transgender"}
+//     ]
+
+//   if (typeof this.props.currentActor.attributes === 'undefined') return <Loader active inline='centered' />
+
+//       return (
+//         <div className="profile-card">
+//           <div style={{textAlign: 'center', fontSize: '2rem'}}>
+//             {first_name}'s Profile
+//           </div>
+
+//           <Grid container style={{'padding-top': '100px'}}>
+//             <Grid.Row centered>
+//               <Grid.Column >
+//               <Form
+//                 onSubmit={this.updateActor}>
+
+//                 <Form.Field
+//                   control={Input}
+//                   width={12}
+//                   name="first_name"
+//                   onChange={this.handleChange}
+//                   value={first_name}
+//                   color="orange"
+//                   label='First Name'/>
+
+//                 <Form.Field
+//                   control={Input}
+//                   width={12}
+//                   name="last_name"
+//                   onChange={this.handleChange}
+//                   value={last_name}
+//                   label='Last Name'/>
+
+//                 <Form.Field
+//                   control={Input}
+//                   width={12}
+//                   name="email"
+//                   onChange={this.handleChange}
+//                   value={email}
+//                   label='E-Mail'/>
+
+//                 <Form.Field
+//                   control={Input}
+//                   width={12}
+//                   name="feet"
+//                   onChange={this.handleChange}
+//                   value={feet}
+//                   placeholder="Feet"
+//                   label='Feet'/>
+
+//                 <Form.Field
+//                   control={Input}
+//                   width={12}
+//                   name="inches"
+//                   onChange={this.handleChange}
+//                   value={inches}
+//                   placeholder="Inches"
+//                   label='Inches'/>
+
+//                 <Form.Field
+//                   control={Input}
+//                   width={12}
+//                   name="vocal_range"
+//                   onChange={this.handleChange}
+//                   value={vocal_range}
+//                   label='Vocal Range'/>
+
+//                 <Form.Select
+//                   control={Select}
+//                   width={12}
+//                   name="equity"
+//                   onChange={(event, state) => this.handleChange(state)}
+//                   label='Equity'
+//                   options={booleans}
+//                   value={equity}
+//                   />
+
+//                 <Form.Field
+//                   control={Select}
+//                   width={12}
+//                   name="gender"
+//                   onChange={(event, state) => this.handleChange(state)}
+//                   value={gender}
+//                   options={genders}
+//                   label='Gender'
+//                 />
+
+//                 <Form.Field
+//                 control={Input}
+//                 width={12}
+//                 name="birthday"
+//                 onChange={this.handleChange}
+//                 value={birthday}
+//                 placeholder="YYYY-MM-DD"
+//                 label='Birthday'/>
+
+//                 <br />
+
+//                 <Button type="submit">Save</Button>
+//               </Form>
+//               </Grid.Column>
+//             </Grid.Row>
+//           </Grid>
+//         </div>
+//       )
+//     }
+//   }
+
+class ActorProfile2 extends Component {
+  handleSubmit = (values, {
+    props = this.props,
+    setSubmitting
+  }) => {
+
+    console.log(values);
+    alert('Form Submitted');
+    setSubmitting(false);
+    return;
   }
 
-  handleChange = (event) => {
-    event.persist();
-    if (event.name) {
-      this.props.updateCurrentActorForm({
-        [event.name]: event.value
-      })
-    } else {
-      this.props.updateCurrentActorForm({
-        [event.target.name]: event.target.value
-      })
-    }
-  }
+render() {
+console.log(this.props)
+  return (
+    <Formik
+      initialValues={{
+        first_name: '',
+        email_address: '',
+        gender: ''
+      }}
+      validate={(values) => {
+        let errors = [];
 
+        if (!values.email)
+          errors.email = "Email Address Required";
 
-  updateActor = (event) => {
-    event.preventDefault()
+        //check if my values have errors
+        return errors;
+      }}
+      onSubmit={this.handleSubmit}
+      render={formProps => {
+        return (
+          <Form>
+            <Field
+              type="text"
+              name="first_name"
+              placeholder="First Name"
+            />
+            <ErrorMessage name="first_name" />
 
-    const { first_name, last_name, email, height, vocal_range, equity, gender, birthday, city } = this.props.currentActor.attributes
+            <Field
+              type="text"
+              name="email"
+              placeholder="Email address"
+            />
+            <ErrorMessage name="email" />
 
-      fetchUpdateCurrentActor(this.props.currentActor.id, {first_name, last_name, email, height, vocal_range, equity, gender, birthday, city }).then(
-        this.setState({ success: !this.state.success }, () => {
-          alert('Profile Saved')
-          this.setState({ success: !this.state.success })
-        })
-      )
-    }
+            <Field
+              name="gender"
+              component="select"
+              placeholder="Your Gender">
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </Field>
 
+            <ErrorMessage name="gender" />
 
-  render() {
+            <button
+              type="submit"
+              disabled={formProps.isSubmitting}>
+              Submit Form
+                 </button>
+          </Form>
+        );
+      }}
+    />);
+}
+}
 
-    const { first_name, last_name, email, feet, inches, vocal_range, equity, gender, birthday } = this.props.currentActor.attributes
-
-    const booleans = [
-      { key: 'true', text: "True", value: true },
-      { key: 'false', text: "False", value: false }
-    ]
-
-    const genders = [
-      {key: 'female', text: "Female", value: "Female"},
-      {key: 'male', text: "Male", value: "Male"},
-      {key: 'Transgender', text: "Transgender", value: "transgender"}
-    ]
-
-  if (typeof this.props.currentActor.attributes === 'undefined') return <Loader active inline='centered' />
-
-      return (
-        <div className="profile-card">
-          <div style={{textAlign: 'center', fontSize: '2rem'}}>
-            {first_name}'s Profile
-          </div>
-
-          <Grid container style={{'padding-top': '100px'}}>
-            <Grid.Row centered>
-              <Grid.Column >
-              <Form
-                onSubmit={this.updateActor}>
-
-                <Form.Field
-                  control={Input}
-                  width={12}
-                  name="first_name"
-                  onChange={this.handleChange}
-                  value={first_name}
-                  color="orange"
-                  label='First Name'/>
-
-                <Form.Field
-                  control={Input}
-                  width={12}
-                  name="last_name"
-                  onChange={this.handleChange}
-                  value={last_name}
-                  label='Last Name'/>
-
-                <Form.Field
-                  control={Input}
-                  width={12}
-                  name="email"
-                  onChange={this.handleChange}
-                  value={email}
-                  label='E-Mail'/>
-
-                <Form.Field
-                  control={Input}
-                  width={12}
-                  name="feet"
-                  onChange={this.handleChange}
-                  value={feet}
-                  placeholder="Feet"
-                  label='Feet'/>
-
-                <Form.Field
-                  control={Input}
-                  width={12}
-                  name="inches"
-                  onChange={this.handleChange}
-                  value={inches}
-                  placeholder="Inches"
-                  label='Inches'/>
-
-                <Form.Field
-                  control={Input}
-                  width={12}
-                  name="vocal_range"
-                  onChange={this.handleChange}
-                  value={vocal_range}
-                  label='Vocal Range'/>
-
-                <Form.Select
-                  control={Select}
-                  width={12}
-                  name="equity"
-                  onChange={(event, state) => this.handleChange(state)}
-                  label='Equity'
-                  options={booleans}
-                  value={equity}
-                  />
-
-                <Form.Field
-                  control={Select}
-                  width={12}
-                  name="gender"
-                  onChange={(event, state) => this.handleChange(state)}
-                  value={gender}
-                  options={genders}
-                  label='Gender'
-                />
-
-                <Form.Field
-                control={Input}
-                width={12}
-                name="birthday"
-                onChange={this.handleChange}
-                value={birthday}
-                placeholder="YYYY-MM-DD"
-                label='Birthday'/>
-
-                <br />
-
-                <Button type="submit">Save</Button>
-              </Form>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      )
-    }
-  }
-
-
-export default withAuth(connect(state => ({ currentActor: state.currentActor }), { loadInitialActorState, updateCurrentActorForm })(ActorProfile))
+export default withAuth(connect(state => ({ currentActor: state.currentActor }), { loadInitialActorState, updateCurrentActorForm })(ActorProfile2))
